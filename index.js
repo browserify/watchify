0bf078bb7c52e41500d36de903531a9efdfd3f9f
Andrew Kelley <superjoe30@gmail.com>
2013-09-22 03:22:27 -0400

    whitespace cleanup

--- index.js
+++ index.js
@@ -12,35 +12,35 @@ function watchify(opts) {
     var pkgcache = {};
     var watching = {};
     var pending = false;
-    
+
     b.on('package', function (file, pkg) {
         pkgcache[file] = pkg;
     });
-    
+
     b.on('dep', function (dep) {
         if (watching[dep.id]) return;
         watching[dep.id] = true;
         cache[dep.id] = dep;
-        
+
         fs.watch(dep.id, function (type) {
             delete cache[dep.id];
             watching[dep.id] = false;
-            
+
             // wait for the disk/editor to quiet down first:
             if (!pending) setTimeout(function () {
                 pending = false;
                 b.emit('update');
             }, opts.delay || 300);
-            
+
             pending = true;
         });
     });
-    
+
     var bundle = b.bundle.bind(b);
     var first = true;
     b.bundle = function (opts_, cb) {
         if (b._pending) return bundle(opts_, cb);
-        
+
         if (typeof opts_ === 'function') {
             cb = opts_;
             opts_ = {};
@@ -50,9 +50,9 @@ function watchify(opts) {
         opts_.includePackage = true;
         opts_.packageCache = pkgcache;
         first = false;
-        
+
         return bundle(opts_, cb);
     };
-    
+
     return b;
 }

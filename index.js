b0a76a3ef571a47555ed279590a12a92be23c826
James Halliday <mail@substack.net>
2014-07-24 19:58:38 -0700

    basic approximation works

--- index.js
+++ index.js
@@ -1,51 +1,22 @@
-var through = require('through');
-var copy = require('shallow-copy');
-var browserify = require('browserify');
+var through = require('through2');
 var fs = require('fs');
 var chokidar = require('chokidar');
 
-module.exports = watchify;
-watchify.browserify = browserify;
-
-function watchify (files, opts) {
-    var b;
-    if (!opts) {
-        opts = files || {};
-        files = undefined;
-        b = typeof opts.bundle === 'function' ? opts : browserify(opts);
-    } else {
-        b = typeof files.bundle === 'function' ? files : browserify(files, opts);
-    }
-    var cache = {};
-    var pkgcache = {};
-    var pending = false;
+module.exports = function (b, opts) {
+    if (!opts) opts = {};
+    var cache = opts.cache || {};
+    var pkgcache = opts.pkgcache || {};
     var changingDeps = {};
-    var first = true;
-    
-    if (opts.cache) {
-        cache = opts.cache;
-        delete opts.cache;
-        first = false;
-    }
-    
-    if (opts.pkgcache) {
-        pkgcache = opts.pkgcache;
-        delete opts.pkgcache;
-    }
-    
-    b.on('package', function (file, pkg) {
-        pkgcache[file] = pkg;
-    });
+    var pending = false;
     
     b.on('dep', function (dep) {
         cache[dep.id] = dep;
-        watchFile(dep.id, dep.id);
+        if (dep.file) watchFile(dep.file);
     });
-    
     b.on('file', function (file) {
         watchFile(file);
     });
-
+    
     var fwatchers = {};
     var fwatcherFiles = {};
     b.on('bundle', function (bundle) {
@@ -105,57 +76,10 @@ function watchify (files, opts) {
         pending = true;
     }
     
-    var bundle = b.bundle.bind(b);
     b.close = function () {
         Object.keys(fwatchers).forEach(function (id) {
             fwatchers[id].forEach(function (w) { w.close() });
         });
-    };
-    
-    b.bundle = function (opts_, cb) {
-        if (b._pending) return bundle(opts_, cb);
-        
-        if (typeof opts_ === 'function') {
-            cb = opts_;
-            opts_ = {};
-        }
-        if (!opts_) opts_ = {};
-        if (!first) opts_.cache = cache;
-        opts_.includePackage = true;
-        opts_.packageCache = pkgcache;
-        
-        // we only want to mess with the listeners if the bundle was created
-        // successfully, e.g. on the 'end' event.
-        var outStream = bundle(opts_, cb);
-        outStream.on('error', function (err) {
-            var updated = false;
-            b.once('update', function () { updated = true });
-            
-            if (err.type === 'not found') {
-                (function f () {
-                    if (updated) return;
-                    fs.exists(err.filename, function (ex) {
-                        if (ex) b.emit('update', [ err.filename ])
-                        else setTimeout(f, opts.delay || 600)
-                    });
-                })();
-            }
-        });
-        
-        var start = Date.now();
-        var bytes = 0;
-        outStream.pipe(through(function (buf) { bytes += buf.length }));
-        outStream.on('end', end);
-        
-        function end () {
-            first = false;
-            
-            var delta = ((Date.now() - start) / 1000).toFixed(2);
-            b.emit('log', bytes + ' bytes written (' + delta + ' seconds)');
-            b.emit('time', Date.now() - start);
-            b.emit('bytes', bytes);
-        }
-        return outStream;
     };
     
     return b;

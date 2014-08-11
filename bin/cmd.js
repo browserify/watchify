2ea9dba8351d822e03a2c8075e0858b270a4ee66
Julian Gruber <julian@juliangruber.com>
2014-08-11 14:03:13 +0200

    prevent listener leak warning

--- bin/cmd.js
+++ bin/cmd.js
@@ -6,6 +6,7 @@ var path = require('path');
 
 var fromArgs = require('./args.js');
 var w = fromArgs(process.argv.slice(2));
+w.setMaxListeners(Infinity);
 
 var outfile = w.argv.o || w.argv.outfile;
 var verbose = w.argv.v || w.argv.verbose;

2be8a192b1412196427cf28a73778c87d2991868
Manuel Cabral <m.cabral@digisfera.pt>
2014-10-30 10:54:37 +0000

    Fixes EPERM error on Windows on rename by writing to output file on callback

--- bin/cmd.js
+++ bin/cmd.js
@@ -15,33 +15,31 @@ if (!outfile) {
     console.error('You MUST specify an outfile with -o.');
     process.exit(1);
 }
-var dotfile = path.join(path.dirname(outfile), '.' + path.basename(outfile));
 
 w.on('update', bundle);
 bundle();
 
 function bundle () {
-    var wb = w.bundle();
-    wb.on('error', function (err) {
-        console.error(String(err));
-        fs.writeFile(outfile, 'console.error('+JSON.stringify(String(err))+')', function(err) {
-            if (err) console.error(err);
-        })
-    });
-    wb.pipe(fs.createWriteStream(dotfile));
-    
-    var bytes, time;
-    w.on('bytes', function (b) { bytes = b });
-    w.on('time', function (t) { time = t });
-    
-    wb.on('end', function () {
-        fs.rename(dotfile, outfile, function (err) {
-            if (err) return console.error(err);
+    w.bundle(function(err, buf) {
+        if(err) {
+           console.error(String(err));
+            fs.writeFile(outfile, 'console.error('+JSON.stringify(String(err))+')', function(err) {
+                if (err) console.error(err);
+            }) 
+        }
+        else {
+            fs.writeFile(outfile, buf, function(err) {
+                if (err) console.error(err);
+            })
             if (verbose) {
-                console.error(bytes + ' bytes written to ' + outfile
+                console.error(buf.length + ' bytes written to ' + outfile
                     + ' (' + (time / 1000).toFixed(2) + ' seconds)'
                 );
             }
-        });
+        }
     });
+    
+    var time = 0;
+    w.on('time', function (t) { time = t });
+    
 }

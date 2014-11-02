9138875bc4d7498370218aea0c13e772100de684
Jake Hiller <jakemhiller@gmail.com>
2014-11-02 14:16:32 -0500

    set max listeners on event emitter instances. Fixes maxListeners warnings when watching multiple bundles.

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ function watchify (b, opts) {
         if (fwatcherFiles[file].indexOf(file) >= 0) return;
         
         var w = chokidar.watch(file, {persistent: true});
+        w.setMaxListeners(0);
         w.on('error', b.emit.bind(b, 'error'));
         w.on('change', function () {
             invalidate(file);
@@ -95,6 +96,7 @@ function watchify (b, opts) {
         if (fwatcherFiles[mfile].indexOf(file) >= 0) return;
 
         var w = chokidar.watch(file, {persistent: true});
+        w.setMaxListeners(0);
         w.on('error', b.emit.bind(b, 'error'));
         w.on('change', function () {
             invalidate(mfile);

8c684af7f44eae55ad1616ea06aa568324808604
Andres Suarez <zertosh@gmail.com>
2015-03-22 23:55:11 -0400

    Use tmpdir for test output

--- test/api_brfs.js
+++ test/api_brfs.js
@@ -9,7 +9,7 @@ var mkdirp = require('mkdirp');
 var split = require('split');
 
 var os = require('os');
-var tmpdir = path.join(__dirname, 'tmp', Math.random() + '');
+var tmpdir = path.join((os.tmpdir || os.tmpDir)(), 'watchify-' + Math.random());
 
 var files = {
     main: path.join(tmpdir, 'main.js'),

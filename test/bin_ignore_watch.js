6d579e93de2c53934db2e5d0b9b693e081f525f9
Andres Suarez <zertosh@gmail.com>
2015-03-29 22:02:33 -0400

    Use "win-spawn" so the tests can run windows

--- test/bin_ignore_watch.js
+++ test/bin_ignore_watch.js
@@ -2,7 +2,7 @@ var test = require('tape');
 var fs = require('fs');
 var path = require('path');
 var mkdirp = require('mkdirp');
-var spawn = require('child_process').spawn;
+var spawn = require('win-spawn');
 var split = require('split');
 
 var cmd = path.resolve(__dirname, '../bin/cmd.js');

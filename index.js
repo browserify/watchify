28f6e9b642282799fed8d933c587c2b59e98e7ad
Jimmy Wärting <jimmy@warting.se>
2021-07-09 13:30:27 +0200

    trim lines

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function watchify (b, opts) {
     var changingDeps = {};
     var pending = false;
     var updating = false;
-    
+
     var wopts = {persistent: true};
     if (opts.ignoreWatch) {
         var ignored = opts.ignoreWatch !== true
@@ -35,7 +35,7 @@ function watchify (b, opts) {
         b.on('reset', collect);
         collect();
     }
-    
+
     function collect () {
         b.pipeline.get('deps').push(through.obj(function(row, enc, next) {
             var file = row.expose ? b._expose[row.id] : row.file;
@@ -47,27 +47,27 @@ function watchify (b, opts) {
             next();
         }));
     }
-    
+
     b.on('file', function (file) {
         watchFile(file);
     });
-    
+
     b.on('package', function (pkg) {
         var file = path.join(pkg.__dirname, 'package.json');
         watchFile(file);
         if (pkgcache) pkgcache[file] = pkg;
     });
-    
+
     b.on('reset', reset);
     reset();
-    
+
     function reset () {
         var time = null;
         var bytes = 0;
         b.pipeline.get('record').on('end', function () {
             time = Date.now();
         });
-        
+
         b.pipeline.get('wrap').push(through(write, end));
         function write (buf, enc, next) {
             bytes += buf.length;
@@ -84,11 +84,11 @@ function watchify (b, opts) {
             this.push(null);
         }
     }
-    
+
     var fwatchers = {};
     var fwatcherFiles = {};
     var ignoredFiles = {};
-    
+
     b.on('transform', function (tr, mfile) {
         tr.on('file', function (dep) {
             watchFile(mfile, dep);
@@ -122,12 +122,12 @@ function watchify (b, opts) {
         fwatchers[file].push(w);
         fwatcherFiles[file].push(dep);
     }
-    
+
     function invalidate (id) {
         if (cache) delete cache[id];
         if (pkgcache) delete pkgcache[id];
         changingDeps[id] = true;
-        
+
         if (!updating && fwatchers[id]) {
             fwatchers[id].forEach(function (w) {
                 w.close();
@@ -135,12 +135,12 @@ function watchify (b, opts) {
             delete fwatchers[id];
             delete fwatcherFiles[id];
         }
-        
+
         // wait for the disk/editor to quiet down first:
         if (pending) clearTimeout(pending);
         pending = setTimeout(notify, delay);
     }
-    
+
     function notify () {
         if (updating) {
             pending = setTimeout(notify, delay);
@@ -150,13 +150,13 @@ function watchify (b, opts) {
             changingDeps = {};
         }
     }
-    
+
     b.close = function () {
         Object.keys(fwatchers).forEach(function (id) {
             fwatchers[id].forEach(function (w) { w.close() });
         });
     };
-    
+
     b._watcher = function (file, opts) {
         return chokidar.watch(file, opts);
     };
